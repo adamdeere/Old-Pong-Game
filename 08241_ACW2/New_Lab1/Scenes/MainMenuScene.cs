e46fab1e3f49893f51bbdecfe8e841e728679d2b
Adam <adam.deere@hotmail.com>
2023-03-10 23:45:20 +0000

    refactored rendering text to the screen into its own class

--- 08241_ACW2/New_Lab1/Scenes/MainMenuScene.cs
+++ 08241_ACW2/New_Lab1/Scenes/MainMenuScene.cs
@@ -1,6 +1,7 @@
 ﻿using OpenTK;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
+using PongGame.Utility;
 using System;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -9,13 +10,7 @@ namespace PongGame
 {
     internal class MainMenuScene : Scene, IScene
     {
-        private Bitmap textBMP;
-        private int textTexture;
-        private Graphics textGFX;
-        public static string serverIp;
-        public static string masterIp;
-        public static int portNo;
-
+        private readonly RenderTextOnScreen m_RenderText;
         public MainMenuScene(SceneManager sceneManager)
             : base(sceneManager)
         {
@@ -24,19 +19,8 @@ namespace PongGame
             // Set the Render and Update delegates to the Update and Render methods of this class
             sceneManager.renderer = Render;
             sceneManager.updater = Update;
-
-            // Create Bitmap and OpenGL texture for rendering text
-            textBMP = new Bitmap(sceneManager.Width, sceneManager.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb); // match window size
-            textGFX = Graphics.FromImage(textBMP);
-            textGFX.Clear(Color.CornflowerBlue);
-            textTexture = GL.GenTexture();
-            GL.Enable(EnableCap.Texture2D);
-            GL.BindTexture(TextureTarget.Texture2D, textTexture);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)All.Linear);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)All.Linear);
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, textBMP.Width, textBMP.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, IntPtr.Zero);
-            GL.BindTexture(TextureTarget.Texture2D, 0);
-            GL.Disable(EnableCap.Texture2D);
+            m_RenderText = new RenderTextOnScreen(sceneManager.Width, sceneManager.Height);
+           
         }
 
         public void Update(FrameEventArgs e)
@@ -53,40 +37,16 @@ namespace PongGame
             GL.LoadIdentity();
             GL.Ortho(0, sceneManager.Width, 0, sceneManager.Height, -1, 1);
 
-            if (textBMP != null)
+            if (m_RenderText.BMP != null)
             {
-                RenderText("welcome to pong", 0f, 0f);
-                RenderText("1. single player game", 0f, 40f);
-                RenderText("2. multyplayer game", 0f, 80f);
-                RenderText("3. host networked game", 0f, 140f);
-                RenderText("4 connect to networked game", 0f, 180f);
-                RenderText("5. display high scores", 0, 220f);
+                m_RenderText.RenderText("welcome to pong", 0f, 0f);
+                m_RenderText.RenderText("1. single player game", 0f, 40f);
+                m_RenderText.RenderText("2. multyplayer game", 0f, 80f);
+                m_RenderText.RenderText("3. host networked game", 0f, 140f);
+                m_RenderText.RenderText("4 connect to networked game", 0f, 180f);
+                m_RenderText.RenderText("5. display high scores", 0, 220f);
             }
         }
-
-        private void RenderText(string text, float x, float y)
-        {
-            textGFX.DrawString(text, new Font("Arial", 20), Brushes.White, x, y);
-
-            // Enable the texture
-            GL.Enable(EnableCap.Texture2D);
-            GL.BindTexture(TextureTarget.Texture2D, textTexture);
-
-            BitmapData data = textBMP.LockBits(new Rectangle(0, 0, textBMP.Width, textBMP.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, (int)textBMP.Width, (int)textBMP.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
-            textBMP.UnlockBits(data);
-
-            GL.Begin(PrimitiveType.Quads);
-            GL.TexCoord2(0f, 1f); GL.Vertex2(0f, 0f);
-            GL.TexCoord2(1f, 1f); GL.Vertex2(sceneManager.Width, 0f);
-            GL.TexCoord2(1f, 0f); GL.Vertex2(sceneManager.Width, sceneManager.Height);
-            GL.TexCoord2(0f, 0f); GL.Vertex2(0f, sceneManager.Height);
-            GL.End();
-
-            GL.BindTexture(TextureTarget.Texture2D, 0);
-            GL.Disable(EnableCap.Texture2D);
-        }
-
         public void SelectMenu(KeyboardKeyEventArgs e)
         {
             KeyboardState keyState = Keyboard.GetState();
---
+++ 08241_ACW2/New_Lab1/Utility/RenderTextOnScreen.cs
@@ -0,0 +1,62 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System;
+using OpenTK;
+
+namespace PongGame.Utility
+{
+    internal class RenderTextOnScreen
+    {
+        private readonly Bitmap textBMP;
+        private readonly int textTexture;
+        private readonly Graphics textGFX;
+
+        private readonly int width, height;
+        public RenderTextOnScreen(int width, int height)
+        {
+            this.width= width;
+            this.height= height;
+            // Create Bitmap and OpenGL texture for rendering text
+            textBMP = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb); // match window size
+            textGFX = Graphics.FromImage(textBMP);
+            textGFX.Clear(Color.CornflowerBlue);
+            textTexture = GL.GenTexture();
+            GL.Enable(EnableCap.Texture2D);
+            GL.BindTexture(TextureTarget.Texture2D, textTexture);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)All.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)All.Linear);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, textBMP.Width, textBMP.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, IntPtr.Zero);
+            GL.BindTexture(TextureTarget.Texture2D, 0);
+            GL.Disable(EnableCap.Texture2D);
+        }
+
+        public Bitmap BMP
+        {
+            get { return textBMP; }
+        }
+
+        public void RenderText(string text, float x, float y)
+        {
+            textGFX.DrawString(text, new Font("Arial", 20), Brushes.White, x, y);
+
+            // Enable the texture
+            GL.Enable(EnableCap.Texture2D);
+            GL.BindTexture(TextureTarget.Texture2D, textTexture);
+
+            BitmapData data = textBMP.LockBits(new Rectangle(0, 0, textBMP.Width, textBMP.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, (int)textBMP.Width, (int)textBMP.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+            textBMP.UnlockBits(data);
+
+            GL.Begin(PrimitiveType.Quads);
+            GL.TexCoord2(0f, 1f); GL.Vertex2(0f, 0f);
+            GL.TexCoord2(1f, 1f); GL.Vertex2(width, 0f);
+            GL.TexCoord2(1f, 0f); GL.Vertex2(width, height);
+            GL.TexCoord2(0f, 0f); GL.Vertex2(0f, height);
+            GL.End();
+
+            GL.BindTexture(TextureTarget.Texture2D, 0);
+            GL.Disable(EnableCap.Texture2D);
+        }
+    }
+}
